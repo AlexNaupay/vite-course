6e21c8347fae46ac65bf14b48d79e0ac58624b1c
Alex Naupay <alexnaupay@gmail.com>
2023-05-13 20:07:41 -0500

    env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
+
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({command, mode})=> {
     const port = 5173;
 
+    const env = loadEnv(mode, process.cwd())
+
+    console.log(env.VITE_NAME)
+
     return {
         server:{
             port
-        }
+        },
+        envPrefix: 'VITE'
+
     }
 })
